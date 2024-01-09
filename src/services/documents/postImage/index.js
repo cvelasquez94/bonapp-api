5fb7780249e844e3d18fa6e48a3e6aafd1f7e30c
Carlos Velasquez <carlosvelasquez@Carloss-MacBook-Pro.local>
2024-01-09 02:04:42 -0300

    fixed parameters for images

--- src/services/documents/postImage/index.js
+++ src/services/documents/postImage/index.js
@@ -6,8 +6,8 @@ const route = async (fastify, opts, next) => {
   const { postImage } = require('./service')(fastify)
 
   fastify.post('/postImage', { schema }, async(request, reply) => {
-    const { url, name, desc, staskInstanceId} = request.body
-    const image = await postImage({ url, name, desc, staskInstanceId})
+    const { url, name, desc, staskInstance_id, user_id} = request.body
+    const image = await postImage({ url, name, desc, staskInstance_id, user_id})
     return reply
       .type('application/json')
       .send({
