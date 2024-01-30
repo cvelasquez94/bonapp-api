8bdfc2a981547a033ed48ae7c9740fc1fd658e93
Cristian Rodrigo Zenteno <cristianzenteno87@gmail.com>
2024-01-30 18:43:44 -0300

    fix dateTime post

--- src/services/checkList/postSTaskInstance/service.js
+++ src/services/checkList/postSTaskInstance/service.js
@@ -1,18 +1,32 @@
-const {  } = require('../../../models')
-const { Op } = require("sequelize");
+const {} = require('../../../models');
+const { Op } = require('sequelize');
 
 module.exports = (fastify) => {
-  const { STaskInstance } = fastify.db
-  async function createSTaskInstance({ subTaskId, userId, status, dateTime, comment, score, photo, dateNow}) {
+  const { STaskInstance } = fastify.db;
+  async function createSTaskInstance({
+    subTaskId,
+    userId,
+    status,
+    dateTime,
+    comment,
+    score,
+    photo,
+    dateNow,
+  }) {
     try {
-      var dateTimeStr = ''
-      var dateSplit = {};
+      let dateTimeStr = '';
+      let dateSplit = {};
       //TODO : en front al formatear string date ddMMyyyy
-      if(dateNow){
-        dateSplit = dateNow.split('/')
+      if (dateTime) {
+        dateSplit = dateTime.split('/');
       }
       if (dateSplit.length == 3) {
-        dateTimeStr = dateSplit[0].padStart(2, "0")+"-"+dateSplit[1].padStart(2, "0")+"-"+dateSplit[2].padStart(4, "0")
+        dateTimeStr =
+          dateSplit[0].padStart(2, '0') +
+          '-' +
+          dateSplit[1].padStart(2, '0') +
+          '-' +
+          dateSplit[2].padStart(4, '0');
       }
       //console.log('str',dateTimeStr)
 
@@ -20,22 +34,30 @@ module.exports = (fastify) => {
       const existingInstance = await STaskInstance.findOne({
         where: {
           [Op.and]: [
-                  { subTask_id: subTaskId,
-                    user_id: userId
-                  },
-                  STaskInstance.sequelize.where(
-                          STaskInstance.sequelize.fn('DATE_FORMAT', STaskInstance.sequelize.col('dateTime')
-                                                    ,'%d-%m-%Y'),
-                                                dateTimeStr),
-          ]
-        }
+            { subTask_id: subTaskId, user_id: userId },
+            STaskInstance.sequelize.where(
+              STaskInstance.sequelize.fn(
+                'DATE_FORMAT',
+                STaskInstance.sequelize.col('dateTime'),
+                '%d-%m-%Y'
+              ),
+              dateTimeStr
+            ),
+          ],
+        },
       });
 
       // Decide si debes actualizar o crear un nuevo registro
       if (existingInstance) {
         // Si viene de auditoría y tiene score o comentario, actualiza
         if (score || comment) {
-          await existingInstance.update({ status, dateTime, comment, score, photo });
+          await existingInstance.update({
+            status,
+            dateTime,
+            comment,
+            score,
+            photo,
+          });
           return existingInstance;
         } else {
           // Si es checklist y no tiene score ni comentario, elimina
@@ -43,7 +65,7 @@ module.exports = (fastify) => {
           return { dateTime: '', id: 0, status: '', subTask_id: 0, user_id: 0 };
         }
       }
-      
+
       const instance = await STaskInstance.create({
         subTask_id: subTaskId,
         user_id: userId,
@@ -51,18 +73,18 @@ module.exports = (fastify) => {
         dateTime,
         comment,
         score,
-        photo
+        photo,
       });
-      if(!instance) {
-        throw new Error('No se puedo crear la instancia')
+      if (!instance) {
+        throw new Error('No se puedo crear la instancia');
       }
-      return instance
+      return instance;
     } catch (error) {
-      throw new Error(error)
+      throw new Error(error);
     }
   }
 
   return {
-    createSTaskInstance
-  }
-}
+    createSTaskInstance,
+  };
+};
