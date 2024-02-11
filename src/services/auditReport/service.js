15eb696c33a1823547a04827ac781f9b3ca3ab99
Fernando Castellino <>
2024-02-11 01:36:22 -0300

    change getDate,Hours, etc with getUTC

--- src/services/auditReport/service.js
+++ src/services/auditReport/service.js
@@ -209,7 +209,7 @@ module.exports = (fastify) => {
       
       doc
         .fontSize(10)
-        .text(`Hora: ${today.getHours().toString().padStart(2, '0')}:${today.getMinutes().toString().padStart(2, '0')}`, doc.x, 50, {
+        .text(`Hora: ${today.getUTCHours().toString().padStart(2, '0')}:${today.getUTCMinutes().toString().padStart(2, '0')}`, doc.x, 50, {
           align: 'right',
         });
       doc.moveDown(7);
@@ -388,15 +388,17 @@ module.exports = (fastify) => {
       //dateNow ahora viene del front dd-mm-yyyy
       if (dateNow) {
         dateTimeStr = dateNow
+        console.log('dateNow: ', dateTimeStr);
       } else {
         //TODO quitar esto dsp de release apk
         now = new Date();
         const offset = now.getTimezoneOffset() * 60000; // Obtener el desplazamiento de la zona horaria en milisegundos
         const localDateTime = new Date(now - offset); // Ajustar la hora al tiempo local
-        dateTimeStr = `${localDateTime.getDate().toString().padStart(2, '0')}-${(localDateTime.getMonth()+1).toString().padStart(2, '0')}-${localDateTime.getFullYear()}`;
+        console.log('localDateTime: ', localDateTime, ' offset: ', offset);
+        dateTimeStr = `${localDateTime.getUTCDate().toString().padStart(2, '0')}-${(localDateTime.getUTCMonth()+1).toString().padStart(2, '0')}-${localDateTime.getUTCFullYear()}`;
       }
 
-      console.log('dateNow: ', dateTimeStr);
+      console.log('dateTimeStr: ', dateTimeStr);
 
     const checkList = await Checklist.findAll({
       where: {
