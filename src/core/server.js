86b71ee45608bb259f6d56fdfa33f1881ba8f775
Carlos Velasquez <carlosvelasquez@Carloss-MacBook-Pro.local>
2024-04-10 19:17:35 -0300

    without cron

--- src/core/server.js
+++ src/core/server.js
@@ -7,7 +7,7 @@ async function start() {
   try {
     const fastify = await app()
     await fastify.listen({ port: config.port, host: '0.0.0.0' })
-    runCronJob()
+    // runCronJob()
   } catch (err) {
     logger.error(err)
     process.exit(1)
